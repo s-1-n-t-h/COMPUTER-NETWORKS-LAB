c61ce935aa7a839e9409db15684c05d2446b428f
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-06-09 15:04:44 +0530

    typos and user interface fixes

--- EXP 7/UDPServer.java
+++ EXP 7/UDPServer.java
@@ -5,17 +5,20 @@ import java.net.InetAddress;
 
 public class UDPServer {
     public static void main(String... args) throws Exception{
+        System.out.println("\nServer is starting...");
         DatagramSocket ds = new DatagramSocket(6363);
         byte[] b1 = new byte[1024];
+        System.out.println("\nServer Started...");
         DatagramPacket dp = new DatagramPacket(b1, b1.length);
         ds.receive(dp);
+        System.out.println("\nData Received from Client");
 
         String str = new String(dp.getData());
         int num = Integer.parseInt(str.trim());
         int result = num*num;
 
         // sending result back to client 
-
+        System.out.println("\nSending data: "+result+" from server to client..");
         // convert data into byte array
         byte[] b2 = Integer.toString(result).getBytes();
         InetAddress ia = InetAddress.getLocalHost();

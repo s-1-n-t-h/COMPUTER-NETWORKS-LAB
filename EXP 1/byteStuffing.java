99e64acd408b3b567c30c7795dedbbdb3a98389a
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-07-01 23:55:18 +0530

    shortend

--- EXP 1/byteStuffing.java
+++ EXP 1/byteStuffing.java
@@ -1,17 +1,15 @@
 package STUFFING;
 import java.util.*;
-class characterStuffing {
+class byteStuffing {
     String std;
     // considering 'E' as escape Sequence and 'F' as flag byte
 
     void charStuff(String data){
         data = "F"+data+"F";
-        int length = data.length();
-        for(int i = 1; i < length-1; i++){ // exclude first and last flag indexes
+        for(int i = 1; i < data.length()-1; i++){ // exclude first and last flag indexes
            if( data.charAt(i) == 'F' || data.charAt(i) == 'E'){
                data = data.substring(0, i) + "E" + data.substring(i);
                i++; // for avoiding pointing to same byte again
-               length = data.length();
            }
         }
         System.out.println("\nStuffed Data: "+data);
@@ -22,27 +20,25 @@ class characterStuffing {
     void charDeStuff(){
         System.out.println("\nReceived message: "+this.std);
         String data = this.std.substring(1,this.std.length()-1);
-        int length = data.length();
-        for (int i = 0; i < length - 1; i++) { // exclude first and last flag indexes
+        for (int i = 0; i < data.length()-1; i++) { // exclude first and last flag indexes
             if ( data.charAt(i) == 'E') {
                 String verify = data.substring(i, i+2);
                 if( verify.equals("EF") || verify.equals("EE")){
                     data = data.substring(0, i) + data.substring(i+1);
                 }
-                length = data.length();
             }
         }
         System.out.println("\nData after de-stuffing: "+data);    
     }
     public static void main(String[] args) throws Exception{
-        characterStuffing cs = new characterStuffing();
+        byteStuffing bs = new byteStuffing();
         System.out.print("\nData: ");
         Scanner sc = new Scanner(System.in);
 
         String data = sc.nextLine();
 
-        cs.charStuff(data);
-        cs.charDeStuff();
+        bs.charStuff(data);
+        bs.charDeStuff();
     }
     
 }
